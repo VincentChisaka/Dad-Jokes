54fc09ef5ed8c2fac2eab9f7294f2420c5084f4f
VincentChisaka <vincentchisaka2@gmail.com>
2023-01-13 10:45:53 +0300

    Login Form JS

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // intro to js//
 //https://icanhazdadjoke.com/ //
 
+const loginPopup = document.querySelector(".login-popup");
+
+window.addEventListener("load", function(){
+
+  showPop();
+  
+})
+
+function showPop() {
+   const timeLimit = 5 // seconds;
+   let i=0;
+   const timer = setInterval(function(){
+    i++;
+    if(i == timeLimit){
+      clearInterval(timer);
+      loginPopup.classList.add("show");
+    }
+    console.log(i)
+   },1000);
+   
+}
+
 document.getElementById("btn").addEventListener("click", joke);
 
 async function joke() {
